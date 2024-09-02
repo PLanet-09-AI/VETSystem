29d98dbdd4512120bac755762219b2e08c005214
PLanet-09AI <ndumisobuthelezi028@gmail.com>
2024-09-02 16:45:29 +0200

    hey

---
+++ BestReg/Data/InventoryItem.cs
@@ -0,0 +1,26 @@
+﻿namespace BestReg.Data
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public InventoryItem Item { get; set; }
+        public int QuantityOrdered { get; set; }
+
+    }
+    public class SupplierOrder
+    {
+        public int Id { get; set; }
+        public string SupplierName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> Items { get; set; }
+    }
+
+}
